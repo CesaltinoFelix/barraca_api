cee5709e306de757b640ff039ef6d20780942828
Reginaldo Angolar <reginaldoangolares@gmail.com>
2023-06-27 20:06:10 +0100

    postgres

--- database/database.js
+++ database/database.js
@@ -1,8 +1,28 @@
 const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+{
+    username: 'postgres',
+    password: 'akualina',
+    database: 'barraca',
+    host: 'localhost',
+    dialect: 'postgres'
+})
 
+const testDbConnection = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  };
+
+/*
 const connection = new Sequelize('barraca','root','', {
     host: 'localhost',
     dialect: 'mysql'
 }); 
+*/
 
-module.exports = connection;
+//module.exports = { sq: sequelize, testDbConnection };
+module.exports = sequelize;

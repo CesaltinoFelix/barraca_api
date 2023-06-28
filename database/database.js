ccafb6035c2241b59fedc41459be58eff694f05d
unknown <edmarangolar@gmail.com>
2023-06-28 11:20:06 +0100

    update index

--- database/database.js
+++ database/database.js
@@ -2,20 +2,12 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
 {
     username: 'postgres',
-    password: 'akualina',
+    password: 'mfalsolution',
     database: 'barraca',
     host: 'localhost',
     dialect: 'postgres'
 })
 
-const testDbConnection = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
-  };
 
 /*
 const connection = new Sequelize('barraca','root','', {

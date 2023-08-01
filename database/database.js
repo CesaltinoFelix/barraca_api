ed71744296047bb26dc81d064b35de6a63639586
Reginaldo Angolar <reginaldoangolares@gmail.com>
2023-08-01 14:42:25 +0100

    security updates

--- database/database.js
+++ database/database.js
@@ -1,14 +1,28 @@
 
 const Sequelize = require('sequelize');
+const path = require('path')
 const fs = require('fs')
-const path = require('path');
-/*const sequelize = new Sequelize('barraca','root','', {
+const caminho = path.resolve(__dirname,'DigiCertGlobalRootCA.crt (1).pem')
+
+
+
+const sequelize = new Sequelize('barraca','root','', {
     host: 'localhost',
     dialect: 'mysql'
 }); 
-*/
-const sslCertPath = path.resolve(__dirname, 'DigiCertGlobalRootCA.crt.pem');
 
+/*
+const sequelize = new Sequelize('barraca','barracabd','naldo_2019', {
+    host: 'barracamysql.mysql.database.azure.com',
+    
+   
+    dialect: 'mysql'
+}); 
+
+*/
+
+
+/*
 const connection = new Sequelize(
 {
     username: 'Reginaldo',
@@ -17,15 +31,17 @@ const connection = new Sequelize(
     host: 'barracaserver.postgres.database.azure.com',
     port:5432,  
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+       
+        
     },
     dialect: 'postgres'
 })
 
-
+*/
 //module.exports = { sq: sequelize, testDbConnection }; ssl:{ca:fs.readFileSync("{pg_hba.conf}")},
-//module.exports = sequelize;
-module.exports = connection;
+module.exports = sequelize;
+//module.exports = connection;
 
 /* 
 const connection = new Sequelize(
--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,111 @@
 const express = require("express");
 const users = require("../database/users");
 const UserValidator = require("../middleware/UserValidator");
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 
 // Route handler using async/await and data validation
+
 router.get("/users", async (req, res) => {
   try {
-    const usersList = await users.findAll({ raw: true, order: [["id", "DESC"]] });
-    res.json(usersList);
+    const user = await users.findAll({ raw: true, order: [["id", "DESC"]] });
+    res.json({id:user.id,name:user.name,email:user.email,img:user.img,entityId:user.entityId,createdAt:user.createdAt});
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get("/login/:email/:password", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
-    const { email, password } = req.params;
-    const user = await users.findOne({ where: { email, password } });
+    const { email, password } = req.body;
+
+    
+
+    const user = await users.findOne({ where: { email } });
+  // var data = JSON.stringify(user);
+    //  console.log(data)
+    
     if (user) {
-      res.json(user);
+   
+      let senhaCriptografada = user.password
+      const senhaCorrespondente = await bcrypt.compare(password, senhaCriptografada);
+      if (senhaCorrespondente) {
+        res.status(200).json({name:user.name,img:user.img,
+          email:user.email,entityId:user.entityId,createdAt:user.createdAt});
+      } else {
+        res.status(401).json({ error: 'Credenciais inválidas.' });
+      }
+
     } else {
-      res.status(401).send("Invalid credentials");
+      res.status(401).send("Credenciais inválidas.");
     }
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    //res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json(error)
   }
 });
 
 router.post("/users", UserValidator.validateUserData, async (req, res) => {
   try {
     const { email, password, img = "", entityId, name } = req.body;
-    const newUser = await users.create({ name, email, password, img, entityId });
-    res.json(newUser);
+    
+    const salt = await bcrypt.genSalt(10);
+   
+    const senha= await bcrypt.hash(password, salt);
+    
+    const newUser = await users.create({ name, email, password:senha, img, entityId });
+   
+    res.status(200).json({name:`${newUser.name}`,email:`${newUser.email}`,
+    img:`${newUser.img}`,entityId:`${newUser.entityId}`,
+    createdAt:`${newUser.createdAt}`});
+
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json({error:"Internal Server Error!"});
   }
 });
+
+router.put("/login",async (req,res)=>
+{
+
+ 
+ try {
+
+  const {email,password}=req.body;
+  
+  const user = await users.findOne({where:{email}})
+ // console.log(user)
+  if(user)
+  {
+    const salt = await bcrypt.genSalt(10);
+    const senha= await bcrypt.hash(password, salt);
+
+    const data = await users.update({password:senha},{where: {email:email}}).then(data=>
+      {
+        res.status(200).json({message:"Data updated!"})
+      })
+    
+  }
+  else
+  {
+    res.status(401).json({message:"Crendencias Inválidas."})
+  }
+
+  
+ } catch (error) {
+    res.status(500).json({message:"Internal Server Error"})
+ }
+
+
+})
+
 
 router.get("/users/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const user = await users.findOne({ where: { id } });
     if (user) {
-      res.json(user);
+      res.json({id:user.id,name:user.name,email:user.email,img:user.img,entityId:user.entityId,createdAt:user.createdAt});
     } else {
       res.redirect("/");
     }
